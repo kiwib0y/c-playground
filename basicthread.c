ac386675d7e48e0d4d22a7ef9b3d26b4405afb0a
KiwiB0y <caki6ko@yahoo.com>
2021-01-24 23:18:10 +0100

    basic threading program, saw it somewhere

---
+++ basicthread.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void *entry_point(void *value) {
+	printf("hello from the second thread :D \n");
+	
+	int *num = (int *) value;
+
+	printf("the value of value is %d\n", *num);
+
+	return NULL;
+}
+
+int main(int argc, char *argv[]) {
+	
+	pthread_t thread;
+
+	printf("hello from the first thread!\n");
+
+	int num = 123;
+
+	pthread_create(&thread, NULL, entry_point, &num);
+
+	pthread_join(thread, NULL);
+
+	return EXIT_SUCCESS;
+}
